53f416e9efb8ec7efd47f786c278e16c275c997d
AdamL-DC <adam.lamoureux@dcmail.ca>
2024-01-29 13:59:11 -0500

    Installed bootstrap and setup file system for lab1

---
+++ DOM-Notes/Form-Validation/script.js
@@ -0,0 +1,11 @@
+console.log("Script ran!");
+
+
+function validateForm() {
+    console.dir(document.forms);
+    let inputValue = document.forms["myForm"]["fname"].value;
+    console.log(inputValue);
+    if(inputValue == "" || inputValue.length > 2){
+        alert("Form validation failed");
+    }
+}
