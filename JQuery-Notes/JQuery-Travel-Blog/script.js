c0b8d01ca1b4a06cd5de3b41e785e877c7ecae1e
AdamL-DC <adam.lamoureux@dcmail.ca>
2024-02-12 20:18:22 -0500

    week 5 class 1 content

---
+++ JQuery-Notes/JQuery-Travel-Blog/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    console.log("JQuery is running");
+    // Injecting an H1 header into the header html element with an id of "header"
+    $("#header").html("<h1>Welcome to our Travel Blog! </h1>");
+
+    // Create Navigation Bar
+    const categories = ["Beaches", "Mountains", "Cities", "Forests", "Deserts"]
+    let navContent = "<ul>"
+    $.each(categories, function (index, category) {
+        //console.log(index);
+        //console.log(category);
+        navContent += `<li onclick="loadCategoryContent('${category}')">${category}</li>`;
+    });
+    navContent += "</ul>";
+    $("#navbar").html(navContent);
+});
+
+window.loadCategoryContent = function (category) {
+    console.log("loadCategoryContent function ran!");
+    console.log(category);
+    let content = `<h2>${category}</h2>`;
+    content += `<div class="carousel" id="${category.toLowerCase()}-carousel"></div>`; 
+    $("#content").html(content);
+
+    // Create a function to populate our Carousel
+    // Create a function to populate our image gallery
+
+};
