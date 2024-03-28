011178f70e814e5bd37792bf489a8aae8d5fbcf3
AdamL-DC <adam.lamoureux@dcmail.ca>
2024-03-28 16:50:08 -0400

    continued pokemon fullstack

--- FullStack-Pokemon/index.js
+++ FullStack-Pokemon/index.js
@@ -1,5 +1,5 @@
-
 const express = require('express');
+const pokemonRouter = require('./routes/pokemon');
 
 const app = express();
 
@@ -10,10 +10,12 @@ app.set("view engine", "ejs");
 app.get("/hello", (req, res) => res.send("Hello World!"));
 
 // We are instructin our applicaiton to look into the routes folder and into the pokemon.js file inside that folder to manage any routes that begin with the path "/"
-app.use("/", require("./routes/pokemon"));
-
-
+app.use("/", pokemonRouter);
 
 app.listen(3500, () => {
 console.log(" Express server listening on port 3500");
 });
+
+
+
+
