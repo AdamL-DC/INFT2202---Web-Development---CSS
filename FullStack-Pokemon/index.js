f8d2a3cbf7b2737843e52a68b8d8045b5056bfec
AdamL-DC <adam.lamoureux@dcmail.ca>
2024-03-28 16:15:48 -0400

    satred pokemon collector

---
+++ FullStack-Pokemon/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+
+const app = express();
+
+app.set("views", "./views"); // Create views folder
+
+app.set("view engine", "ejs");
+
+app.get("/hello", (req, res) => res.send("Hello World!"));
+
+// We are instructin our applicaiton to look into the routes folder and into the pokemon.js file inside that folder to manage any routes that begin with the path "/"
+app.use("/", require("./routes/pokemon"));
+
+
+
+app.listen(3500, () => {
+console.log(" Express server listening on port 3500");
+});
---
+++ FullStack-Pokemon/routes/pokemon.js
@@ -0,0 +1,4 @@
+
+// we are initializing a router object from express router
+const router = express.Router();
+
